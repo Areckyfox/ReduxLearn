59ec876f7f8e0a32f9b6c8cc2414dbe05831234c
Arkadiusz Lis <alis@ARTE206>
2018-11-15 14:34:37 +0100

    add dispatch wich adding result to array and to the list

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,29 +1,41 @@
-const initialState ={
-    counter: 0
-}
+const initialState = {
+  counter: 0,
+  results: []
+};
 
 const reducer = (state = initialState, action) => {
-    if(action.type === "INCREMENT"){
-        return {
-            counter: state.counter +1 
-        }
-    }
-    if(action.type === "DECREMENT"){
-        return {
-            counter: state.counter - 1
-        }
-    }
-    if(action.type === "ADD"){
-        return {
-            counter: state.counter + 5
-        }
-    }
-    if(action.type === "SUBTRACT"){
-        return {
-            counter: state.counter - 5
-        }
-    }
-    return state; 
-}
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        ...state,
+        counter: state.counter + 1
+      };
+
+    case "DECREMENT":
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+
+    case "ADD":
+      return {
+        ...state,
+        counter: state.counter + action.val
+      };
+
+    case "SUBTRACT":
+      return {
+           ...state,
+            counter: state.counter - action.val
+         };
+      case "STORE_RESULT":
+      return {
+          ...state,
+          results: state.results.concat({id: new Date(), value: state.counter})
+      }    
+  }
+
+  return state;
+};
 
 export default reducer;
