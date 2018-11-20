7dfac7cff1d53cbf780eb4f005868f7fd7feb6bb
Arkadiusz Lis <alis@ARTE206>
2018-11-20 16:53:59 +0100

    reducer

--- redux--assignment-2-problem/src/components/AddPerson/AddPerson.js
+++ redux--assignment-2-problem/src/components/AddPerson/AddPerson.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import './AddPerson.css';
+import "./AddPerson.css";
 
-const addPerson = (props) => (
-    <div className="AddPerson">
-        <button onClick={props.personAdded}>Add Person</button>
-    </div>
-);
+class AddPerson extends Component {
+  state = {
+    name: "",
+    age: ""
+  };
 
-export default addPerson;
+  nameChangeHandler = event => {
+    this.setState({
+      name: event.target.value
+    });
+  };
+
+  ageChangeHandler = event => {
+    this.setState({
+      age: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="AddPerson">
+        <button onClick={()=> this.props.personAdded(this.state.name, this.state.age)}>Add Person</button>
+        <input
+          value={this.state.name}
+          type="text"
+          placeholder="Name"
+          onChange={this.nameChangeHandler}
+        />
+        <input
+          value={this.state.age}
+          type="number"
+          placeholder="Age"
+          onChange={this.ageChangeHandler}
+        />
+      </div>
+    );
+  }
+}
+
+export default AddPerson;
